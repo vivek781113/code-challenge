7eecb485410ddd953c3ee99eeb09bfb6697395e0
Vivek Tiwary <Vivek.Tiwary@Honeywell.com>
2022-04-20 11:49:27 +0530

    added exception filter attribute for catching exception at action level

---
+++ RefactorThis/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using refactor_me.Filters;
+using System.Web.Http;
+
+namespace refactor_this
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.Filters.Add(new ProductsExceptionFilter());
+
+            // Web API configuration and services
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+            formatters.JsonFormatter.Indent = true;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RefactorThis/Filters/ProductsExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+namespace refactor_me.Filters
+{
+    public class ProductsExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            string exceptionMessage;
+            if (actionExecutedContext.Exception.InnerException == null)
+            {
+                exceptionMessage = actionExecutedContext.Exception.Message;
+            }
+            else
+            {
+                exceptionMessage = actionExecutedContext.Exception.InnerException.Message;
+            }
+            //We can log this exception message to the file or database.
+            Debug.WriteLine(exceptionMessage);
+            var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("An unhandled exception was thrown by service."),
+                    ReasonPhrase = "Internal Server Error.Please Contact your Administrator."
+            };
+            actionExecutedContext.Response = response;
+        }
+    }
+}
